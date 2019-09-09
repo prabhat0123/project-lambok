f8b14b6dd748cfaa3671ac98ebcf235bbf4e3234
Prabhat <prabhat.kum@ascendcorp.com>
2019-09-09 13:15:44 +0700

    @slf4j added

--- src/main/java/com/peekay/lambok/LambokApplication.java
+++ src/main/java/com/peekay/lambok/LambokApplication.java
@@ -3,10 +3,14 @@ package com.peekay.lambok;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class LambokApplication {
 
 	public static void main(String[] args) {
+		log.debug("This message is due to @slf4j ");
 		SpringApplication.run(LambokApplication.class, args);
 	}
 
